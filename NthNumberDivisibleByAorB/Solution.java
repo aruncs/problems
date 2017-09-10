d98f825c5e47c54c9781b9948aecf5bbd689f883
Arun C S <acspayyeri@gmail.com>
2017-09-10 10:41:35 +0530

    Solution for NthNumberDivisibleByAorB

---
+++ NthNumberDivisibleByAorB/Solution.java
@@ -0,0 +1,36 @@
+public class Solution{
+  public static void main(String[] args) {
+
+    System.out.println(getNthNumberDivisibleByAorB(2,3,10));
+    System.out.println(getNthNumberDivisibleByAorB(3,4,10));
+    System.out.println(getNthNumberDivisibleByAorB(7,2,10));
+  }
+
+  public static long getNthNumberDivisibleByAorB(long a,long b,long n){
+    long min;
+    if(a < b){
+      min = a;
+    }else{
+      min = b;
+    }
+    long product = a * b;
+
+    long count = 0;
+    long lastNumber = min;
+    long number = min;
+    while(count < n){
+      if(number % a == 0){
+        count++;
+        lastNumber = number;
+      }else if(number % b == 0){
+        count++;
+        lastNumber = number;
+      }else if(number % product == 0){
+        count++;
+        lastNumber = number;
+      }
+      number++;
+    }
+    return lastNumber;
+  }
+}
